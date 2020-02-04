19fd7ff12f9f5327a80187632a3b4582196ec7c2
coastlines <efaulkenberry@gmail.com>
2020-02-03 19:17:54 -0600

    checkpoint side nav, main widget layouts

---
+++ src/checkpoint1/sidenav.js
@@ -0,0 +1,22 @@
+    const menuIconEl = $('.header-menu-icon');
+    const sidenavEl = $('.side-nav');
+    const sidenavCloseEl = $('.side-nav-close-icon');
+
+    // Add and remove provided class names
+    function toggleClassName(el, className) {
+      if (el.hasClass(className)) {
+        el.removeClass(className);
+      } else {
+        el.addClass(className);
+      }
+    }
+    // Open the side nav on click
+    menuIconEl.on('click', function() {
+      toggleClassName(sidenavEl, 'active');
+    });
+
+    // Close the side nav on click
+    sidenavCloseEl.on('click', function() {
+      toggleClassName(sidenavEl, 'active');
+    });
+
